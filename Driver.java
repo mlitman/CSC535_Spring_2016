beaed716da3ceda06904b3cc727ecde0da2687c3
Mike Litman <mike@awesomefat.com>
2016-02-03 22:00:33 -0600

    made a change to Driver.java

--- Driver.java
+++ Driver.java
@@ -3,6 +3,19 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		
+		int count = 0;
+		for(int i = 1; i <= 1000; i = i + 1)
+		{
+			if(i % 2 == 0)
+			{
+				System.out.println("i is even");
+				count = count + 1;
+			}
+			else
+			{
+				System.out.println("i is odd");
+			}
+		}
+		System.out.println(count);
 	}
 }
